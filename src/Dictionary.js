5e47df6b13ff6cdced441d242710a1ee6731dbe9
annafry95 <annafry95@gmail.com>
2026-01-16 14:27:05 +0000

    Removed alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,7 +13,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} defintion`);
 
     let apiKey = "fbeo6b3af9d08bb94a5d7e475ct03604";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
