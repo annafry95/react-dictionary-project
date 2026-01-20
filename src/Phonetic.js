088f5eb4c05fa157e9fc31484098e4e9732b566d
annafry95 <annafry95@gmail.com>
2026-01-20 13:05:37 +0000

    Added phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <span className="text">{props.phonetic}</span>
+    </div>
+  );
+}
